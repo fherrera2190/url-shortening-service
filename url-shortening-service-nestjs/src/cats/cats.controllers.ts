d9b1c85f32c42bba7d227c3dd6c42775233b215b
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-02-07 21:43:27 -0300

    add cats endpoint

--- url-shortening-service-nestjs/src/cats/cats.controllers.ts
+++ url-shortening-service-nestjs/src/cats/cats.controllers.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Redirect, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   @HttpCode(HttpStatus.PARTIAL_CONTENT)
-  findAll(@Res() res: Response) {
-    void res;
+  // findAll(@Res() res: Response) {
+  findAll() {
+    //void res;
     // res
     //   .status(HttpStatus.PARTIAL_CONTENT)
     //   .send({ message: 'This action returns all cats' });
     return { message: 'This action returns all cats' };
+  }
+
+  @Get('abcd/*')
+  // @Redirect('http://www.googl.com')
+  findAll2() {
+    return 'This route uses a wildcard';
   }
 }
